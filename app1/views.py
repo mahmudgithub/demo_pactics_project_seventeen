55f5cebfe665b4c6a11829c77dae1d16b53dfcc5
mahmud555 <mahmudhossain838@gmail.com>
2020-01-29 17:02:19 +0600

    first_comit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+#from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+
+from . import models
+from . import serializers
+
+class UserListView(generics.ListAPIView):
+    queryset = models.CustomUser.objects.all()
+    serializer_class = serializers.UserSerializer
